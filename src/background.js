fa0b7883e54989874b4090a3af8b34e0648e7d7a
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2017-10-22 00:10:52 +0100

    Add functionality to remember last "view all" change

--- src/background.js
+++ src/background.js
@@ -1,18 +1,29 @@
 var any_language_regex = /games-workshop.com\/\w{2}-\w{2}/;
+var view_all_query_string = "&view=all";
 var my_language_code, my_language_string, my_language_regex;
+var view_all;
 
-function getMyLanguage() {
+function getPreferences() {
     chrome.storage.local.get(function(obj) {
         my_language_code = obj['gwLang'] || 'en-GB';
         my_language_string = 'games-workshop.com/' + my_language_code;
         my_language_regex = new RegExp(my_language_string);
+        view_all = obj['gwViewAll'] || false;
     });
 }
 
 function checkAndRedirect(details) {
     if (!details.url.match(my_language_regex) && details.url.match(any_language_regex)) {
         return {redirectUrl: details.url.replace(any_language_regex, my_language_string)};
-    }
+    };
+    if (details.url.includes("product.endDate")) {
+        if (view_all && !details.url.endsWith(view_all_query_string)) {
+            return {redirectUrl: details.url + view_all_query_string};
+        }
+        else if (!view_all && details.url.endsWith(view_all_query_string)) {
+            return {redirectUrl: details.url.replace(view_all_query_string, "")};
+        };
+    };
 }
 
 var url_filters = {
@@ -20,10 +31,10 @@ var url_filters = {
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    chrome.storage.local.set(request, getMyLanguage);
+    chrome.storage.local.set(request, getPreferences);
 });
 
-getMyLanguage();
+getPreferences();
 chrome.webRequest.onBeforeRequest.addListener(
     checkAndRedirect,
     url_filters,
--- src/contentscript.js
+++ src/contentscript.js
@@ -6,3 +6,15 @@ $("body").on("click", "a[href*='" + locale_link_identifier + "']", function(e) {
     e.preventDefault();
     chrome.runtime.sendMessage({gwLang: language_code}, function() {location.reload()});
 });
+
+$("body").on("click", "a[href*='product.endDate'].read-more", function(e) {
+    e.preventDefault();
+    if (this.href.endsWith('view=all')) {
+        // currently viewing all, so turning it off
+        viewAll = false;
+    }
+    else {
+        viewAll = true;
+    }
+    chrome.runtime.sendMessage({gwViewAll: viewAll}, function() {location.reload()});
+});
