9b51f8a76d9b51e9406c4e7eb7a9c654e4673516
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2018-06-02 22:45:38 +0100

    Calculate and store full browse page urls; exclude certain ones by name

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,5 +1,6 @@
 import json
 import re
+from collections import defaultdict
 
 import requests
 from bs4 import BeautifulSoup
@@ -9,15 +10,24 @@ class Breadcrumbs(object):
     output_file_path = 'breadcrumbs.json'
     product_ranges = {'www.games-workshop.com': ['Warhammer', 'Warhammer-40-000']}
     regions = ['en-GB']
+    banned_entry_names = ['Pre-orders', 'New releases', 'Last chance to buy']
 
     def __init__(self, *args, **kwargs):
         self.browse_pages = []
+        self.product_pages = defaultdict(list)
 
     def assemble(self):
         for region in self.regions:
             for website, product_ranges in self.product_ranges.items():
                 for product_range in product_ranges:
                     self.get_browse_pages_for_product_range(product_range, website, region)
+        print('')
+        print('Found {} browse pages in total'.format(len(self.browse_pages)))
+
+        for browse_page in self.browse_pages:
+            pass
+            # TODO add browse_page to the list for each product page linked to from it
+
 
     def get_browse_pages_for_product_range(self, product_range, website, region):
         params = {'website': website, 'product_range': product_range, 'region': region}
@@ -25,7 +35,7 @@ class Breadcrumbs(object):
         print('Fetching {}'.format(url))
         response = requests.get(url)
         if response.status_code == 200:
-            print('  Parsing the result')
+            print('  Parsing the nav for browse pages')
             soup = BeautifulSoup(response.content, 'html.parser')
             start_phrase = "gw.cartridgeManager.renderPage("
             script_source = soup.find(text=re.compile(start_phrase.replace("(", "\(")))
@@ -34,7 +44,11 @@ class Breadcrumbs(object):
             browse_pages = []
             for category in data['contents'][0]['secondaryContent'][0]['contents'][0]['navigation']:
                 for entry in category['refinements']:
-                    browse_pages.append((entry['properties']['name'], entry['navigationState']))
+                    name = entry['properties']['name']
+                    browse_url = '{url}{nav_state}&view=all'.format(url=url, nav_state=entry['navigationState'])
+                    if name not in self.banned_entry_names:
+                        browse_pages.append((name, browse_url))
+
             self.browse_pages.extend(browse_pages)
             print('  Found {} browse pages'.format(len(browse_pages)))
 
