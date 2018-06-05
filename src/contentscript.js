9f7f22cf159221c1846b976b004c988e461f6088
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2018-06-05 23:08:13 +0100

    Handle breadcrumbs data not being available

--- src/contentscript.js
+++ src/contentscript.js
@@ -38,15 +38,17 @@ $(function() {
 
 $(function() {
     var partial_url = window.location.href.split(/\/\w{2}-\w{2}\//)[1];
-    var page_breadcrumbs = breadcrumbs[partial_url];
-    if (page_breadcrumbs != undefined) {
-        var $main = $("#main");
-        var $nav = $('<div style="text-align: center; padding-top: 15px;"></div>')
-        page_breadcrumbs.forEach(function(e) {
-            var $nav_item = $("<a>" + e[0] + "</a>");
-            $nav_item.attr("href", e[1]).addClass("new-button");
-            $nav.append($nav_item);
-        });
-        $main.prepend($nav);
+    if (typeof breadcrumbs !== "undefined") {
+        var page_breadcrumbs = breadcrumbs[partial_url];
+        if (page_breadcrumbs != undefined) {
+            var $main = $("#main");
+            var $nav = $('<div style="text-align: center; padding-top: 15px;"></div>')
+            page_breadcrumbs.forEach(function(e) {
+                var $nav_item = $("<a>" + e[0] + "</a>");
+                $nav_item.attr("href", e[1]).addClass("new-button");
+                $nav.append($nav_item);
+            });
+            $main.prepend($nav);
+        }
     }
 });
